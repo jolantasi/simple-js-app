430dd0fa97a13d0dc94d4208f073941bf0331473
jolantasi <jolanta.germany@gmail.com>
2024-10-17 19:44:51 +0200

    Update scripts.js
    Created first list of Pokemons with their data.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'Açai';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
+    {name: 'Pikachu', height: '0.4', types: ['electric']},
+    {name: 'Pichu', height: '0.3', types: ['electric']},
+];
