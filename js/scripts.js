1c2434f2c205be8e6e92aa3a4d893be7f988fc78
jolantasi <jolanta.germany@gmail.com>
2024-12-18 07:18:29 +0100

    Refactor Pokémon repository with IIFE and encapsulation
    Update pokemonRepository to use IIFE with getAll and add methods
    
    - Encapsulated pokemonList inside an IIFE
    - Added getAll() method to retrieve Pokémon data
    - Added add() method to add new Pokémon
    - Updated forEach loop to access Pokémon through getAll()

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
-// Array-list of Pokemons
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-    { name: 'Pikachu', height: 0.4, types: ['electric'] },
-    { name: 'Pichu', height: 0.3, types: ['electric'] },
-];
+// Create a new variable to hold the IIFE's returned value
+let pokemonRepository = (function() {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+        { name: 'Pikachu', height: 0.4, types: ['electric'] },
+        { name: 'Pichu', height: 0.3, types: ['electric'] },
+    ];
 
-pokemonList.forEach(pokemon => {
+    // Define the getAll function to return all Pokémon
+    function getAll() {
+        return pokemonList;
+    }
+
+    // Define the add function to add a single item to the pokemonList
+    function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
+// Use getAll() to iterate over the Pokémon in the repository
+pokemonRepository.getAll().forEach(function (pokemon) {
     console.log('name:', pokemon.name);
     console.log('height:', pokemon.height);
 
@@ -20,4 +38,3 @@ pokemonList.forEach(pokemon => {
 
     document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + message + '</p>');
 });
-
